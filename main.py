4936be17bdd98ba1c8e28158f05a2238df8d6f46
riyan <tunggalrahayu1@gmail.com>
2021-09-18 12:38:45 +0700

    first commit

---
+++ main.py
@@ -0,0 +1,29 @@
+import webbrowser
+import pyautogui
+from time import sleep
+
+
+import pandas as pd
+
+# your word to tweet
+data = ['hello', 'hi']
+
+# your username twitter
+username = ''
+
+webbrowser.open("https://twitter.com/"+username)
+
+sleep(5)
+
+def tweet(text):
+    pyautogui.click("img/tomboltweet.png")
+    sleep(2)
+    df=pd.DataFrame([text])
+    df.to_clipboard(index=False,header=False)
+    pyautogui.hotkey("ctrl", "v")
+    pyautogui.click("img/tweet.png")
+
+
+for d in data:
+    tweet(str(d))
+    sleep(2)
